e23d675b4d3a8aaa89b087f5c714c0bbb9e8615a
Mani Alipour <mani.developer4@gmail.com>
2024-07-08 20:27:44 +0330

    "Updated styles for SectionHero components, including responsive font sizes and positioning"

--- src/components/sectionHero/index.tsx
+++ src/components/sectionHero/index.tsx
@@ -25,7 +25,9 @@ const SectionHero = ({}: Props) => {
         <span className={styles.price}>$14.99</span>
       </PurchaseButton>
       {/* Story link */}
-      <StoryLink className="absolute bottom-10 left-[48%] cursor-pointer">
+      <StoryLink
+        className={`absolute bottom-10 xl:left-[48%] cursor-pointer lg:left-[45%] md:left-[40%] left-[35%]`}
+      >
         <span>the story</span>
         <Image src="/images/scroll down.svg" alt="" width={40} height={40} />
       </StoryLink>
@@ -38,20 +40,32 @@ const SectionHero = ({}: Props) => {
 // Hero title component
 const HeroTitle = ({ children }: { children: ReactNode }) => (
   <div>
-    <span className="text-6xl">{children}</span>
+    <span
+      className={`xl:text-6xl lg:text-5xl md:text-4xl text-3xl ${styles.heroTitle}`}
+    >
+      {children}
+    </span>
   </div>
 );
 
 // Hero subtitle component
 const HeroSubtitle = ({ children }: { children: ReactNode }) => (
   <div>
-    <span>{children}</span>
+    <span
+      className={`xl:text-lg lg:text-base md:text-sm text-xs ${styles.heroSubtitle}`}
+    >
+      {children}
+    </span>
   </div>
 );
 
 // Purchase button component
 const PurchaseButton = ({ children }: { children: ReactNode }) => (
-  <button className="section-hero-price-btn">{children}</button>
+  <button
+    className={`section-hero-price-btn xl:text-lg lg:text-base md:text-sm text-xs ${styles.purchaseButton}`}
+  >
+    {children}
+  </button>
 );
 
 // Story link component
